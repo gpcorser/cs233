7abfedf1250146845ce771b4b5cde1a59d949da5
George Corser, CS-233, Kali <gpcorser@svsu.edu>
2025-02-20 09:46:46 -0500

    feb20 linux practice quiz day updates

---
+++ e11/basics/basics/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.w3resource.com/bash-script-exercises/understanding-basic-commands.php
+echo hello world
+echo "hello world"
---
+++ e12/greetfile.sh
@@ -0,0 +1,3 @@
+echo enter your name
+read name
+echo "Hello, $name, today is $(date)" > output.txt
